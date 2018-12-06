5bf8675650de609de652bdf972f8ae4c8b3a6557
Beatriz Lino <beatrizcarlos207@gmail.com>
2018-12-06 12:00:51 -0200

    Add files via upload

---
+++ estrutura-de-dados/listas/lista-de-exercicios-02/Capitulo-02/Hello.java
@@ -0,0 +1,14 @@
+class Hello{
+ public static void main(String[] args) {
+ /// capitulo 2.14 - questao 01
+ System.out.println("nao importa onde estamos nossa mente e nosso lar");
+
+ ///questao 02
+ System.out.println("As vezes voce nao ve que a melhor coisa que ja aconteceu com voce esta ali, bem embaixo do seu nariz.");
+ System.out.println("Quando tiver que escolher entre estar certo e ser gentil, escolha ser gentil.");
+
+ ///questao 03
+
+ System.out.print("Nao precisamos dos olhos para amar, certo?\nApenas sentimos dentro de nos.");
+ }
+ }
